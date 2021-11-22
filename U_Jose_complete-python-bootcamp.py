f4694c2cd32ea6174506d43c81755b1dab90b2e5
Ilya <66158563+Ilya-87@users.noreply.github.com>
2021-11-22 22:14:01 +0300

    new

--- U_Jose_complete-python-bootcamp.py
+++ U_Jose_complete-python-bootcamp.py
@@ -27,12 +27,23 @@
 # print(b[1:4])
 # print(b[-5:-2])
 
-c = 'Nice day'
-print(c)
-c = 'it was ' + c
-print(c)
-print(c[7:12])
-print(c[:6] + ' bad ' + c[12:])
-c=c[:6] + ' bad ' + c[12:]
-c=c*2
-print(c)
+# c = 'Nice day'
+# print(c)
+# c = 'it was ' + c
+# print(c)
+# print(c[7:12])
+# print(c[:6] + ' bad ' + c[12:])
+# c=c[:6] + ' bad ' + c[12:]
+# c=c*2
+# print(c)
+
+c = 'The {v} {n} {d}'
+print(c.format(v='very', n='nice', d='day',))
+
+b = 'The {} {} {}'
+print(b.format('very', 'nice', 'day',))
+
+q= 100
+w = 45
+result = q/w
+print(result)
