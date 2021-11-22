ea37d1e3daab0a46c880eb611f3d4ac1a1499f29
Ilia87 <94649332+Ilia87@users.noreply.github.com>
2021-11-22 17:22:57 +0300

    new

--- U_Jose_complete-python-bootcamp.py
+++ U_Jose_complete-python-bootcamp.py
@@ -3,7 +3,22 @@
 # b = 3.1
 # print(a+a)
 # print(type(a), type(b))
+#
+# print("I'm going")
+# print('hi \tman')
+#
+# print(len('few           fw'))
 
-print("I'm going")
-print('hi \tman')
-
+a = 'abcdefghijk'
+print(a)
+print(a[:3], ' a[:3] ')
+print(a[:-3], ' a[:-3] ')
+print(a[-3:], ' a[-3:] ')
+print(a[::3], ' a[::3] ')
+print(a[3::], ' a[3::] ')
+print(a[3:], ' a[3:] ')
+print(a[::2], ' a[::2]')
+print(a[::-1], ' a[::-1]')
+print(a[::-2], ' a[::-2]')
+print(a[:2], ' a[:2]')
+print(a[2:], ' a[2:]')
