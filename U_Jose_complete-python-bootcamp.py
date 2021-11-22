3a18a77fa22e6e0da233680d3eefc9c8f373c862
Ilia87 <94649332+Ilia87@users.noreply.github.com>
2021-11-22 18:11:45 +0300

    new

--- U_Jose_complete-python-bootcamp.py
+++ U_Jose_complete-python-bootcamp.py
@@ -9,20 +9,30 @@
 #
 # print(len('few           fw'))
 
-a = 'abcdefghijk'
-print(a)
-print(a[:3], ' a[:3] ')
-print(a[:-3], ' a[:-3] ')
-print(a[-3:], ' a[-3:] ')
-print(a[::3], ' a[::3] ')
-print(a[3::], ' a[3::] ')
-print(a[3:], ' a[3:] ')
-print(a[::2], ' a[::2]')
-print(a[::-1], ' a[::-1]')
-print(a[::-2], ' a[::-2]')
-print(a[:2], ' a[:2]')
-print(a[2:], ' a[2:]')
+# a = 'abcdefghijk'
+# print(a)
+# print(a[:3], ' a[:3] ')
+# print(a[:-3], ' a[:-3] ')
+# print(a[-3:], ' a[-3:] ')
+# print(a[::3], ' a[::3] ')
+# print(a[3::], ' a[3::] ')
+# print(a[3:], ' a[3:] ')
+# print(a[::2], ' a[::2]')
+# print(a[::-1], ' a[::-1]')
+# print(a[::-2], ' a[::-2]')
+# print(a[:2], ' a[:2]')
+# print(a[2:], ' a[2:]')
+#
+# b = 'tinker'
+# print(b[1:4])
+# print(b[-5:-2])
 
-b = 'tinker'
-print(b[1:4])
-print(b[-5:-2])
+c = 'Nice day'
+print(c)
+c = 'it was ' + c
+print(c)
+print(c[7:12])
+print(c[:6] + ' bad ' + c[12:])
+c=c[:6] + ' bad ' + c[12:]
+c=c*2
+print(c)
