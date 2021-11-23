0c3425606ceeb9692728e9f66a6d1f0e226a6b23
Ilya <66158563+Ilya-87@users.noreply.github.com>
2021-11-23 21:07:22 +0300

    new

--- calculator binary.py
+++ calculator binary.py
@@ -1,10 +1,14 @@
-base = int(input('выберите систему исчисления от 2 до 10: '))
+# https://youtu.be/b8m9uRMpKJk
+# 60 minute
+base: int = int(input('выберите систему исчисления от 2 до 10: '))
 x = y = int(input(f'десятечное число для перевода в {base}-ую систему: '))
 while x > 0:
     digit = x % base
-    # print(digit, end=' ')
+    str(print(digit, end=''))
     x //= base
-print(f'число ' + str(y) + ' в ' + str(base) + '-ой системе = ' + str(digit))
+print(f'число ' + str(y) + ' в ' + str(base) + '-ой системе = ' + str(x))
 
-print(f'справочно, число {y} в \n2-ой: ' + str(bin(y)) + ', \n8-ой: ' + str(oct(y)),
-      ', \n16-ой: ' + str(hex(y)))
+print(f'справочно, число {y} в '
+      f'\n2-ой: {str(bin(y))}, '
+      f'\n8-ой: {str(oct(y))}, '
+      f'\n16-ой: {str(hex(y))}')
