57e5893c77e2469c34245fe0169c46b99cea856d
Ilia87 <94649332+Ilia87@users.noreply.github.com>
2021-11-25 20:33:10 +0300

    new

--- U_Jose_complete-python-bootcamp.py
+++ U_Jose_complete-python-bootcamp.py
@@ -51,26 +51,53 @@
 # x, y = input('your name: '), float(input('your age: '))
 # print("I'm %s and my age is %d years old.\nHowever age is %5.4f" % (x, y, y))
 # 
+#
+# old_list = [1, 2, 3, 4, 5, 6]
+# print(old_list)
+# print(len(old_list))
+# print(old_list[0])
+# new_list = old_list[:]
+# new_list.append(7)
+# print(new_list)
+# new_list2 = new_list.copy()
+# new_list2.pop(0)
+# print(new_list2)
+# print(old_list + new_list)
+# big_list = old_list[:] + new_list.copy()
+# print(big_list)
+# big_list.sort()
+# print(big_list)
+# big_list.reverse()
+# print(big_list)
+# matrix = [old_list, new_list, new_list2, big_list]
+# print(matrix)
+# print(matrix[3])
+# first_col = [row[0] for row in matrix]
+# print(first_col[1:])
 
-old_list = [1, 2, 3, 4, 5, 6]
-print(old_list)
-print(len(old_list))
-print(old_list[0])
-new_list = old_list[:]
-new_list.append(7)
-print(new_list)
-new_list2 = new_list.copy()
-new_list2.pop(0)
-print(new_list2)
-print(old_list + new_list)
-big_list = old_list[:] + new_list.copy()
-print(big_list)
-big_list.sort()
-print(big_list)
-big_list.reverse()
-print(big_list)
-matrix = [old_list, new_list, new_list2, big_list]
-print(matrix)
-print(matrix[3])
-first_col = [row[0] for row in matrix]
-print(first_col[1:])
+# Dictionaries
+
+d = {'c1': ['a', 'b', 'c', 'd', 'e'], 'c2': [1, 2, 3, 4, 5], 'c3': {'c3_1': ['z', 'x']}}
+print(d)
+print(d.keys())
+print(d.values())
+print(d.items())
+print(d['c3']['c3_1'][1])
+d['c3']['c3_1'][1] = 'g'
+print(d['c3']['c3_1'][1])
+d['c3']['c3_1'][1].upper()
+print(d['c3']['c3_1'][1])
+d['c2'] += {'name'}
+# d['c2']['name'] = {}
+
+print(d)
+print(d['c2'])
+print(d.items())
+list = list(d.items())
+list1 = [1,1,2,2,3,4,5,6,1,1]
+print(list1)
+set(list1)
+print(set(list1))
+x=set(list1)
+x.remo(2)
+print(x)
