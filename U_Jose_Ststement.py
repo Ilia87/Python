c26d2485086f1954fb972e461d6773f05cc84699
Ilia87 <94649332+Ilia87@users.noreply.github.com>
2021-12-07 13:15:11 +0300

    new

--- U_Jose_Ststement.py
+++ U_Jose_Ststement.py
@@ -1,5 +1,32 @@
-list1 = [1,2,3,4,5,6,7,8,9,10]
+# list1 = [1,2,3,4,5,6,7,8,9,10]
+#
+# for i in list1:
+#     # Check for even
+#     if i % 2 == 0:
+#         print(i)
+#     else:
+#         pass
+#     print(f'Odd Number: {i}')
 
-for i in list1:
-    if i % 2 == 0:
-        print(i)
+# list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# list_sum = 1
+# for i in range(1,10,1):
+#     list_sum2 = list_sum
+#     list_sum = list_sum * i
+#     print(f'list_sum = {list_sum}, list_sum2 = {list_sum2}')
+#
+# for i in 'Hello':
+#     print('Hi')
+#
+# tup = (1,2,3)
+# for i in tup:
+#     print(i)
+#
+list3 = [(1,2,3), (4,5,6), (7,8,9), (10,11,12)]
+print(len(list3))
+
+for a, b, c in list3:
+    print(a)
+    print(b)
+    print(c)
+    # print(b)
