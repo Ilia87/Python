92586764fad26099a0dee6d6366a4bfd19ee7d43
Ilia87 <94649332+Ilia87@users.noreply.github.com>
2021-12-15 10:31:29 +0300

    new

--- U_Jose_03-Methods and Functions.py
+++ U_Jose_03-Methods and Functions.py
@@ -23,21 +23,45 @@
 #     print(list1)
 #     return list1
 #
-# check_even([1,45,64,64,213,7,46,13,6,4,63,6,6,654,654,])
+# # check_even([1,45,64,64,213,7,46,13,6,4,63,6,6,654,654,])
+#
+# list_tup1 = [('Abby',100),('Billy',400),('Cassie',800)]
+#
+# def tup(list_tup):
+#     salary_max = 0
+#     name_emp = ''
+#     for emp,salary in list_tup:
+#         if salary > salary_max:
+#             salary_max = salary
+#             name_emp = emp
+#         else:
+#             pass
+#     print((name_emp, salary_max))
+#     return (name_emp, salary_max)
+#
+# tup(list_tup1)
 
-list_tup1 = [('Abby',100),('Billy',400),('Cassie',800)]
+# from random import shuffle
+#
+# # Initial List
+# mylist = [' ','O',' ']
+#
+# def shuffle_list(mylist):
+#     shuffle(mylist)
+#     print(mylist)
+#     return mylist
+#
+def player_guess():
+    guess = ''
+    while guess not in ['0', '1', '2']:
+        guess = input('Please wrire 0, 1, 2:  ')
+    print(guess)
+    return int(guess)
 
-def tup(list_tup):
-    salary_max = 0
-    name_emp = ''
-    for emp,salary in list_tup:
-        if salary > salary_max:
-            salary_max = salary
-            name_emp = emp
-        else:
-            pass
-    print((name_emp, salary_max))
-    return (name_emp, salary_max)
-
-tup(list_tup1)
-
+# def game(mylist, guess):
+#     if mylist[guess] == '0':
+#         'You win'
+#     else:
+#         print('Try again')
+#         print(mylist)
+#     return game()
