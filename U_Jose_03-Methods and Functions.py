f373b812099f26794abe7c7644161d8968c21afb
Ilia87 <94649332+Ilia87@users.noreply.github.com>
2021-12-17 21:53:06 +0300

    new

--- U_Jose_03-Methods and Functions.py
+++ U_Jose_03-Methods and Functions.py
@@ -44,24 +44,141 @@
 # from random import shuffle
 #
 # # Initial List
-# mylist = [' ','O',' ']
+# mylist = ['8','O','8']
 #
 # def shuffle_list(mylist):
 #     shuffle(mylist)
-#     print(mylist)
+#
 #     return mylist
 #
-def player_guess():
-    guess = ''
-    while guess not in ['0', '1', '2']:
-        guess = input('Please wrire 0, 1, 2:  ')
-    print(guess)
-    return int(guess)
-
-# def game(mylist, guess):
-#     if mylist[guess] == '0':
-#         'You win'
+# def player_guess():
+#     guess = ''
+#     while guess not in ['0', '1', '2']:
+#         guess = input("Please write 0, 1, 2:  ")
+#
+#     return int(guess)
+#
+#
+# def check_guess(mylist, guess):
+#     if mylist[guess] == 'O':
+#         print('Correct Guess!')
 #     else:
-#         print('Try again')
+#         print('Wrong! Better luck next time')
 #         print(mylist)
-#     return game()
+#
+#
+# mix = shuffle_list(mylist)
+# gue = player_guess()
+# check_guess(mix, gue)
+
+# def lesser_of_two_evens(a, b):
+#     if a % 2 == 0 and  b % 2 == 0:
+#         if a > b:
+#             print(b)
+#             return b
+#         else:
+#             print(a)
+#             return a
+#     else:
+#         if a < b:
+#             print(b)
+#             return b
+#         else:
+#             print(a)
+#             return a
+# lesser_of_two_evens(10,810)
+
+# def animal(text):
+#     list_text = list(text.split(" "))
+#     print (list_text[0][0] == list_text[1][0])
+#     return list_text[0][0] == list_text[1][0]
+#     #     print('True')
+#     #     return "True"
+#     # else:
+#     #     print('False')
+#     #     return "False"
+#
+# animal("robot rak")
+#
+# def check(a,b):
+#     print (a == 20 or b == 20 or a+b == 20)
+#     return a == 20 or b == 20 or a+b == 20
+#     # if a == 20 or b == 20 or a+b == 20:
+#     #     print('True')
+#     #     return "True"
+#     # else:
+#     #     print('False')
+#     #     return "False"
+#
+# check(55555555555555555,20)
+
+# def old_macdonald(name):
+    # new_name = ''
+    # list_mac = list(name)
+    # list_mac.pop(3)
+    # list_mac.insert(3,"D")
+    # list_mac.pop(0)
+    # list_mac.insert(0,"M")
+    # for l in list_mac:
+    #     new_name +=l
+    # print(new_name)
+
+# def old_macdonald(name):
+#     if len(name)>4:
+#         print(name[:3].capitalize() + name[3:].capitalize())
+#         return name[:3].capitalize()
+#     else:
+#         print("it is too short")
+#
+#
+# old_macdonald('macdonald')
+
+# def rev_t(text):
+#     text = "".join(reversed(text))
+#     print(text)
+# rev_t('We are ready')
+#
+# def rev_t1(text1):
+#     text_list = list(text1.split(" "))
+#     text_list.reverse()
+#     print(' '.join(text_list))
+# rev_t1('We are ready')
+#
+# def rev_t2(text2):
+#     text2 = " ".join(text2.split()[::-1])
+#     print(text2)
+# rev_t2('We are ready')
+#
+# def almost_there(n):
+#     print (n > 10 and n < 100 or n>200)
+#     print((abs(100 - n) <= 10) or (abs(200 - n) <= 10))
+#
+# almost_there(209)
+
+def has_33(nums):
+    list_n = []
+    for n in range(1, len(nums)+1):
+        list_n.append(n)
+    # print(list_n)
+    lisrd = []
+    for i in nums:
+        lisrd.append(i == 3 and nums[nums[i]-1] == 1 or i == 3 and nums[nums[i]+1] == 1)
+    # print(lisrd)
+    # print(nums)
+    list_f = list(zip(list_n, nums, lisrd))
+    # print(list_f)
+    for i0, i1, i2 in  list_f:
+        print(f'в проверке № {i0}, число {i1} и вот результат {i2}')
+
+gu = [1, 3, 1, 3, 1, 7, 1, 3, 6, 7, 1, 3, 3, 1, 3, 1, 6, 7, 1, 3, 3, 1, 6, 7, 9]
+
+def has_3(nyms):
+    for i in range(0, len(nyms)-1):
+        # print(nyms[i:i+1] == [3,3] or nyms[i-1:i] == [3,3])
+        if nyms[i:i + 2] == [3, 3]:
+            print(True)
+        print(False)
+
+has_33(gu)
+has_3(gu)
+
