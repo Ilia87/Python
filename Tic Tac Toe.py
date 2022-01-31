0d0af71965fabb4d4dd9382ca3ae1b8bf09e87b1
Ilia87 <94649332+Ilia87@users.noreply.github.com>
2022-01-31 21:03:48 +0300

    new

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -1,12 +1,18 @@
 def display_board(board):
-
-
+    print('Welcome to Tic Tac Toe!')
+    print('This is your board:')
     print(board[7] + '|' + board[8] + '|' + board[9])
     print('-' * 5)
     print(board[4] + '|' + board[5] + '|' + board[6])
     print('-' * 5)
     print(board[1] + '|' + board[2] + '|' + board[3])
 
+board = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+display_board(board)
 
-test_board = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-display_board(test_board)
+
+def player_x_o():
+    choice
+    player1 = input('Do you want to be X or O: ')
+    print()
+
