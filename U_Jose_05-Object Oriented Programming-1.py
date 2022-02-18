4f3804596201462297a05b0cdf035289aae7ccbd
Ilia87 <94649332+Ilia87@users.noreply.github.com>
2022-02-19 00:41:50 +0300

    new

--- U_Jose_05-Object Oriented Programming-1.py
+++ U_Jose_05-Object Oriented Programming-1.py
@@ -206,42 +206,96 @@
 # print(c.area)
 # print(c.radius)
 # print(c.getCircumference())
+#
+# ## Fill in the Line class methods to accept coordinates as a pair of tuples and return the slope and distance of the line.
+# class Line:
+#     def __init__(self, coor1, coor2):
+#         self.coor1 = coor1
+#         self.coor2 = coor2
+#
+#     def distance(self):
+#        return ((self.coor2[0] - self.coor1[0])**2 + (self.coor2[1] - self.coor1[1])**2)**0.5
+#
+#     def slope(self):
+#         return (self.coor2[1] - self.coor1[1]) / (self.coor2[0] - self.coor1[0])
+#
+#
+# coordinate1 = (3, 2)
+# coordinate2 = (8, 10)
+# li = Line(coordinate1, coordinate2)
+#
+# print(li.distance())
+# print(li.slope())
+#
+#
+# class Cylinder:
+#     pi = 3.14
+#     def __init__(self, height=1,radius=1):
+#         self.height = height
+#         self.radius = radius
+#
+#     def volume(self):
+#         return self.radius**2 * self.pi * self.height
+#
+#     def Area(self):
+#         return 2*self.radius * self.pi * (self.height+self.radius)
+#
+#
+# c = Cylinder(2, 3)
+#
+# print(c.volume())
+# print(c.Area())
 
-## Fill in the Line class methods to accept coordinates as a pair of tuples and return the slope and distance of the line.
-class Line:
-    def __init__(self, coor1, coor2):
-        self.coor1 = coor1
-        self.coor2 = coor2
 
-    def distance(self):
-       return ((self.coor2[0] - self.coor1[0])**2 + (self.coor2[1] - self.coor1[1])**2)**0.5
+class Account:
+    def __init__(self, owner, balance, password=1111):
+        self.owner = owner
+        self.balance = balance
+        self.password = password
 
-    def slope(self):
-        return (self.coor2[1] - self.coor1[1]) / (self.coor2[0] - self.coor1[0])
+    def deposit(self):
+        dep_amount = int(input('Please, enter the amount of your deposit: '))
+        self.balance += dep_amount
+        print(f' You have deposited {dep_amount}rub. Your balance is {self.balance}rub.')
+        return self.balance
+
+    def withdraw(self):
+        dep_amount = int(input('Please, enter the amount of your withdraw: '))
+        self.balance -= dep_amount
+        print(f' You have withdrawn {dep_amount}rub. Your balance is {self.balance}rub.')
+        return self.balance
+
+    def checking_balance(self):
+        if self.balance > 0:
+            return True
+        else:
+            return False
+
+    def finish(self):
+        finish_operations = input(f'{self.owner}, do you wanna finish operations with your account, y or n:')
+        if finish_operations == 'y':
+            return True
+        else:
+            return False
 
 
-coordinate1 = (3, 2)
-coordinate2 = (8, 10)
-li = Line(coordinate1, coordinate2)
 
-print(li.distance())
-print(li.slope())
+acct1 = Account('Ilya', 100)
 
+print('Welcome to the Bank')
 
-class Cylinder:
-    pi = 3.14
-    def __init__(self, height=1,radius=1):
-        self.height = height
-        self.radius = radius
-
-    def volume(self):
-        return self.radius**2 * self.pi * self.height
-
-    def Area(self):
-        return 2*self.radius * self.pi * (self.height+self.radius)
+while True:
+    name = input('Please, enter name: ')
+    password = input('Please, enter password: ')
+    while True:
+        if name == acct1.owner and password == acct1.password:
+            operation = True
+        else:
+            operation = False
 
 
-c = Cylinder(2, 3)
+print('Account owner: ', acct1.owner, '\n'
+      'Account balance:', acct1.balance)
 
-print(c.volume())
-print(c.Area())
+acct1.deposit()
+acct1.withdraw()
