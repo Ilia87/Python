41b97331bcfdb411b78cb3ff31ab9b5b49243af3
Ilia87 <94649332+Ilia87@users.noreply.github.com>
2022-02-20 22:21:18 +0300

    new

---
+++ U_Jose_06_myprogram.py
@@ -0,0 +1,9 @@
+from U_Jose_06_mymodule import my_func
+
+my_func()
+
+from MyMainPackage import some_main_script
+from MyMainPackage.SubPackage import my_subscript
+
+some_main_script.report_main()
+my_subscript.sub_report()
