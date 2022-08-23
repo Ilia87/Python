e976bdcd872d47ba7c06ef178a9dce4184cb11bd
alekseizuev <47051191+alekseizuev@users.noreply.github.com>
2022-08-23 12:16:05 +0300

    Update Fibonacci numbers.py
    Вариант решения при помощи генераторов

--- FA_BRD_GZPN.py
+++ FA_BRD_GZPN.py
@@ -1,14 +1,19 @@
 import re
 
-with open('FA_BRD_GZPN.txt', mode='r', encoding='UTF8') as f:
+bad_chars = ['001_999_000013_0000`.`', "`;"]
+
+with open('all tables.txt', mode='r', encoding='UTF8') as f:
     lines = f.readlines()
     text = ""
     for line in lines:
-        l = line.replace("\n", "");
+        l = line.replace("\n", "")
         text += l
 
-    patterns = re.findall("(LOAD.*?;)", text);
-    print("LENGTH %s" % (str(len(patterns))))
+    patterns = re.findall("(001_999_000013_0000.*?;)", text)
+    #print("LENGTH %s" % (str(len(patterns))))
     for p in patterns:
+        for i in bad_chars:
+            p = p.replace(i, "")
         print(p)
+    print(type(p))
 
---
+++ U_Jose_02-Python Statements (repeating).py
@@ -0,0 +1,24 @@
+
+##Use for, .split(), and if to create a Statement that will print out words that start with 's':
+
+# st = 'Print only the words that start with s in this sentence'
+# lst = []
+# lsts = []
+# for i in st.split(" "):
+#    lst.append(i)
+# for w in lst:
+#     if "s" in w[0]:
+#         lsts.append(w)
+#
+# print(lst)
+# print(lsts)
+#
+# # Recomendation
+# for word in st.split(" "):
+#     if word[0] == "s":
+#         print(word)
+
+# Use range() to print all the even numbers from 0 to 10.
+
+for n in range(0, 12, 2):
+    print(n)
